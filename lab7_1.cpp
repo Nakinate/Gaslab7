2e58a70481d5c77220cf4175b22927f7e27722fd
Nakinate Buatip <nakinate1710@gmail.com>
2021-11-30 14:06:26 +0700

    completelab7_1

---
+++ lab7_1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string func1(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += x[L-i-1];
+		i++;
+	}
+	return y;
+}
+
+string func2(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += toupper(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+string func3(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += tolower(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+int main(){
+    string x;
+    cout << "Input text: ";
+    cin >> x;
+    cout << "Reversed text: "<< func1(x)<<endl;
+    cout << "Palindrome: ";
+    if (func1(func3(x)) == func3(x))
+    {
+        cout << "Yes";
+    }else{
+        cout <<"No" ;
+    }
+    
+    return 0;
+}
+
